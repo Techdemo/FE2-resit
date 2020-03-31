4ab132e81ab103a2ed98c09ffcabbb79f1eaf671
Marcel <fleuren.marcel@gmail.com>
2020-03-31 12:34:03 +0200

    added intersection observer

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,28 @@
-console.log("joejoe")
-function fetch(event){
-  event.preventDefault()
-  alert('event preventDefault')
-}
+document.addEventListener("DOMContentLoaded", function () {
+  // TODO: schrijf hier een scope berichtje over.
+  var lazyCards = [].slice.call(document.querySelectorAll("li.lazy"));
+
+  //TODO: progressive enhancement, check of intersectionobserver beschikbaar is voor je browser.
+  if ("IntersectionObserver" in window) {
+    // TODO: hier kun je ook een begrip op toepassen
+    let lazyCardObserver = new IntersectionObserver(function (entries, observer) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          let lazyCard = entry.target;
+          let lazyImage = lazyCard.querySelector('img')
+
+          lazyImage.src = lazyImage.dataset.src;
+          lazyCard.classList.remove("lazy");
+          lazyCard.classList.add("fade-in")
+          lazyCardObserver.unobserve(lazyCard, lazyImage);
+        }
+      });
+    });
+
+    lazyCards.forEach(function (lazyImage) {
+      lazyCardObserver.observe(lazyImage);
+    });
+  } else {
+    return null
+  }
+});
